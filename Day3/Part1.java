52056ade9391775c80f25a051a9e684cebcce21e
Aqib Rahman <83845832+Init-mb46@users.noreply.github.com>
2022-12-12 22:15:17 +0000

    day 3 part 1

--- Day3/Part1.java
+++ Day3/Part1.java
@@ -4,7 +4,32 @@ import java.util.*;
 import java.io.*;
 
 class Part1 {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    File f = new File("input.txt");
+    FileReader fr = new FileReader(f);
+    BufferedReader br = new BufferedReader(fr);
+    String line;
+    int total = 0;
+    
+    while ((line = br.readLine()) != null) {
+      String l = line.substring(0, line.length() / 2);
+      String r = line.substring(line.length() / 2);
+      boolean bre = false;
+      for (char c : l.toCharArray()) {
+        if (bre) break;
+        for (char z : r.toCharArray()) {
+          if (c == z) {
+            int x = c >= 97 ? c - 96 : c - 38;
+            total += x;
+            bre = true;
+            break;
+          }
+        }
+      }
+    }
+
+    br.close();
+    System.out.println(total);
     
   }
 }
